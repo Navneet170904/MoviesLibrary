e48db14cc11cefa109bef4930e251aa84e481312
Navneet Kumar Yadav <92726948+Navneet170904@users.noreply.github.com>
2024-06-03 22:25:56 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -8,7 +8,7 @@ const Signup = () => {
   const navigate = useNavigate()
   const handleSignup = async (e) => {
     e.preventDefault();
-    const response = await fetch('http://localhost:5000/auth/register', {
+    const response = await fetch('https://movieslibrarybackend.onrender.com/auth/register', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
