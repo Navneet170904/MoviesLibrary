46c070d080e45bea96a2f461aac521a4f3c2aa05
Navneet Kumar Yadav <92726948+Navneet170904@users.noreply.github.com>
2024-06-03 22:58:03 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -14,6 +14,7 @@ const Login = () => {
     e.preventDefault();
     const response = await fetch('https://movieslibrarybackend.onrender.com/auth/login', {
       method: 'POST',
+      mode: 'no-cors',
       headers: {
         'Content-Type': 'application/json',
       },
